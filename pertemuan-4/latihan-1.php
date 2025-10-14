8004b2fa1a6768314a2f87f83fac3468f240ca1e
Bagus Maulana <bagusmaulana0320@gmail.com>
2025-10-14 18:37:36 +0700

    contoh soal latihan 4

---
+++ pertemuan-4/latihan-1.php
@@ -0,0 +1,4 @@
+<?Php
+For ($bil1= 1; $bil1 <= 25; $bil1++)
+Print ("$bil1 ");
+?>
---
+++ pertemuan-4/latihan-2.php
@@ -0,0 +1,8 @@
+<?php
+$name=array("Anggi","Rendi","Bimo","Endar");
+foreach ($name as $nilai)
+{
+echo "$nilai";
+echo "<br />";
+}
+?>
